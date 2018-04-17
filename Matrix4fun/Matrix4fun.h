f7e6cd7752c4156a394feabbe0dafa619bd5d386
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2018-04-17 18:17:40 +0200

    Initial Commit

---
+++ Matrix4fun/Matrix4fun.h
@@ -0,0 +1,19 @@
+//
+//  Matrix4fun.h
+//  Matrix4fun
+//
+//  Created by Pawel Kowalczuk on 17/04/2018.
+//  Copyright © 2018 Applause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Matrix4fun.
+FOUNDATION_EXPORT double Matrix4funVersionNumber;
+
+//! Project version string for Matrix4fun.
+FOUNDATION_EXPORT const unsigned char Matrix4funVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Matrix4fun/PublicHeader.h>
+
+
